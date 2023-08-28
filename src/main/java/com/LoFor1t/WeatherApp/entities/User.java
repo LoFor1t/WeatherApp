dd6485ce728fb95ae688e2a19e3025908c832ec3
LoFor1t <vgresnyh@gmail.com>
2023-08-28 18:26:07 +0200

    Create User Entity and Repository for it. Create an example file for application.properties.

---
+++ src/main/java/com/LoFor1t/WeatherApp/entities/User.java
@@ -0,0 +1,21 @@
+package com.LoFor1t.WeatherApp.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String password;
+}
