5dbf2695b6f49e4b0b563d52ad5ed741c23c47f6
LoFor1t <vgresnyh@gmail.com>
2023-08-29 12:15:28 +0200

    Create validation in form for already existing users. Create html page for sign in. Create post request, with logic for creating new user.

--- src/main/java/com/LoFor1t/WeatherApp/controllers/UsersController.java
+++ src/main/java/com/LoFor1t/WeatherApp/controllers/UsersController.java
@@ -1,16 +1,38 @@
 package com.LoFor1t.WeatherApp.controllers;
 
+import com.LoFor1t.WeatherApp.entities.User;
+import com.LoFor1t.WeatherApp.repositories.UserRepository;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller()
+@Controller
 @RequestMapping("users")
 public class UsersController {
+    private final UserRepository userRepository;
+
+    public UsersController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @GetMapping("/new")
     public String registrationForm(Model model) {
         return "newuser";
+    }
+
+    @PostMapping
+    public String createNewUser(@RequestParam("login") String login, @RequestParam("password") String password, Model model) {
+        String encryptedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        User newUser = User.builder().login(login.toLowerCase()).password(encryptedPassword).build();
+        if (userRepository.existsByLogin(login.toLowerCase())) {
+            model.addAttribute("userExist", true);
+            return "newuser";
+        }
+        userRepository.save(newUser);
+        return "signin";
     }
 }
--- src/main/java/com/LoFor1t/WeatherApp/entities/User.java
+++ src/main/java/com/LoFor1t/WeatherApp/entities/User.java
@@ -15,7 +15,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String name;
+    private String login;
 
     private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
 }
